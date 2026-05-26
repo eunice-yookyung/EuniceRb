31d303f7f0c57dff25eca472b8f55349d038a627
eunice-yookyung <eunicelee@fas.harvard.edu>
2026-05-26 15:57:58 -0400

    light scattering updates

--- DAMOP2026/Doublons/plot_data_damop.m
+++ DAMOP2026/Doublons/plot_data_damop.m
@@ -5,13 +5,46 @@ folder = 'damop_plots';
 rfplot_damop();
 fig = gcf;
 fig2pdf(fig, sprintf('%s/fig_rf_process', folder), is_pdf)
+%%
+n = 2;
+[tileObj, data, files] = plot_csv_data_damop(2);
+rfplot_damop(tileObj)
+nexttile(5)
+ylabel('Observed normalized scattering')
+ax=gca;
+ax.Children(4).XData(end)=nan;
+ax.Children(2).XData(end)=nan;
+% ax.YLim = [0,1];
+nexttile(6)
+ylabel('Elastic scattering per atom')
+ax=gca;
+ax.Children(4).XData(end)=nan;
+ax.Children(2).XData(end)=nan;
+%%
+h = figure;
+set(h, 'Units', 'inches', 'Position', [2, 2, 3.5, 3.5])
+black = [0,0,0];
+darkRed= red * .9;
+col = [black; red];
+for i = [5,3]
+    errorbar(data{i}.hole_frac(1:end-1), data{i}.(2)(1:end-1), data{i}.(3)(1:end-1), ...
+        '.', 'capsize', 0, 'linewidth', 1, 'markerSize', 10)
+    hold on
+end
+for i = 1:2
+    plot(data{2}.hole_frac, data{2}.(i+1), 'LineWidth', 1)
+end
+set(gca, 'ColorOrder', [black;darkRed;black;darkRed])
+ylim([-.1,2.1]), yticks(0:.5:2)
+text(.4,.2,'Singlon','Color','k'), text(.4,1.5,'Doublon','Color',darkRed), xlabel('Hole fraction'), ylabel('Normalized elastic scattering')
+fig2pdf(h, sprintf('%s/dicke', folder), is_pdf)
 
 %% -------------------
 % harmonic oscillator
 is_pdf = 0; % 0 = jpg, 1 = pdf
 
-colors = load("C:\Users\eunic\EuniceRb\DAMOP2026\Doublons\my_colors.mat").colors_teal_brown;
-colors = flipud(colors(5:end, :));
+colors = load("C:\Users\eunic\EuniceRb\DAMOP2026\Doublons\my_colors.mat").colors_blue_red;
+colors = (colors(1:6, :));
 
 x = -5:.01:5;
 f = @(x_, n_) 1/sqrt(2.^n_ * factorial(n_)) * pi^(-1/4) .* exp(-x_.^2 / 2) .* hermiteH(n_, x_);
